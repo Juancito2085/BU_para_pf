d73c44598e29e24455baf28c4b62212311cf49eb
Juancito2085 <jcbrunello@hotmail.com>
2024-07-03 15:58:20 -0300

    Prueba

--- ML_streamlit/main.py
+++ ML_streamlit/main.py
@@ -70,11 +70,15 @@ def entrada_seleccionada(modelo):
 
 entrada, cantidad=entrada_seleccionada(modelo_seleccionado)
 
+#Creamos las columnas
+col1, col2 = st.columns([1, 1])
 
 if modelo_seleccionado=="Predicción de crecimiento":
     img, mapa = plot_predictions_for_categories(entrada,clf)
-    st.image(img, caption='Gráfico de Predicciones por Ciudad', use_column_width=True)
-    folium_static(mapa)
+    with col1:
+        st.image(img, caption='Gráfico de Predicciones por Ciudad', use_column_width=True)
+    with col2:
+        folium_static(mapa)
 else:
     img, mapa = plot_predictions_for_city(entrada,clf,cantidad)
     st.image(img, caption='Gráfico de Predicciones por Categoría' ,use_column_width=True)

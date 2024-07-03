ce566bb03f149ccbdf3aa24797a0020266ae3552
Juancito2085 <jcbrunello@hotmail.com>
2024-07-03 10:57:22 -0300

    prueba

--- ML_streamlit/funciones.py
+++ ML_streamlit/funciones.py
@@ -114,7 +114,7 @@ def plot_predictions_for_categories(categorias1, model):
     plt.xticks(rotation=45)
     plt.subplots_adjust(left=0.1, right=0.75, top=0.9, bottom=0.2)
     plt.show()
-    map_center = [resultado_lat_lon['latitud'].mean(), resultado_lat_lon['longitud'].mean()]
+    '''map_center = [resultado_lat_lon['latitud'].mean(), resultado_lat_lon['longitud'].mean()]
     mapa = folium.Map(location=map_center, zoom_start=12)
     for _, row in resultado_lat_lon.iterrows():
         folium.Marker(
@@ -122,7 +122,7 @@ def plot_predictions_for_categories(categorias1, model):
             popup=row['ciudad']
         ).add_to(mapa)
     mapa.save('mapa_ciudades.html')
-    return mapa
+    return mapa'''
 
 def plot_predictions_for_city(ciudad, model, cantidad=1):
     predicciones = []

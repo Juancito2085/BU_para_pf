997f762087ae3a65bfc972646c9c55a9acbd6160
Juancito2085 <jcbrunello@hotmail.com>
2024-07-04 06:20:31 -0300

    Prueba

--- ML_streamlit/main.py
+++ ML_streamlit/main.py
@@ -15,11 +15,11 @@ import requests
 from streamlit_folium import folium_static
 
 # Lee el contenido del archivo styles.css y aplica el estilo
-with open("ML_streamlit/Datos/styles.css") as f:
+with open("ML_streamlit/styles.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Muestra la imagen usando una ruta relativa
-st.image("ML_streamlit/Datos/Logo.jpeg", use_column_width=True)
+st.image("ML_streamlit/Logo.jpeg", use_column_width=True)
 st.markdown("""
     
     <div class="header">
